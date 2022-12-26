600a84beb4d17e087e9ad9bd75d44fa8d7dce5f0
einSelbst <silvio@mailbox.org>
2022-12-26 22:09:06 +0100

    Revert "wip: do i need this"
    This reverts commit af5d9f37b9724b82fff296979eaa9c317bde2a41.

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -13,7 +13,7 @@ type AppPropsWithLayout = AppProps & {
 
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   // Use the layout defined at the page level, if available
-  const getLayout = Component.getLayout ?? ((page: any) => page)
+  const getLayout = Component.getLayout ?? (page => page)
 
   return getLayout(<Component {...pageProps} />)
   /* return <Component {...pageProps} /> */
